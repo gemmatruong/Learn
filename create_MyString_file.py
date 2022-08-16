58854fe0b033fb1d0c9619fb2e9b212965ba7257
Gemma <gemmatruong99@gmail.com>
2022-08-15 21:52:26 -0700

    create_MyString_file.py

--- create_MyString_file.py
+++ create_MyString_file.py
@@ -1,6 +1,9 @@
 class MyString:
     def __init__(self,s):
         self.string = s
+    
+    def __str__(self):
+        return self.string
 
     def len(self):
         count = 0
@@ -8,13 +11,20 @@ class MyString:
             count += 1
         return count
 
-    def add_string(self, new_str):
-        return "{}{}".format(self.string, new_str)
+    def __add__(self, other):
+        return self.string + other.string
+    
+    def substring(self,start,end,step):
+        substr = ''
+        for i in range(start,end,step):
+            if i >= MyString.len(self):
+                break
+            substr = substr + self.string[i]
+        return substr
 
-
-
-
-str_1 = MyString('Hello my world!')
+str_1 = MyString('Hello Gemma!')
+str_2 = MyString(' Nice to meet you.')
 
 print(str_1.len())
-print(str_1.add_string(' Loving you'))
+print(str_1 + str_2)
+print(str_1.substring(0,3,1))
