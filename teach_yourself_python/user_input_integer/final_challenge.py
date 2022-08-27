769035b8f608a53da7320efc5de14c3aca72c757
Gemma <gemmatruong99@gmail.com>
2022-08-26 23:38:58 -0700

    Add exercises about user input integer

---
+++ teach_yourself_python/user_input_integer/final_challenge.py
@@ -0,0 +1,7 @@
+def guess():
+    print("========== Let's guess the number ===========")
+    guess = int(input("Guess a number between 1 and 10: "))
+    print("You have guessed", guess)
+
+
+guess()
---
+++ teach_yourself_python/user_input_integer/task3.py
@@ -0,0 +1,13 @@
+def sum():
+    print("This is a program that will perform addition")
+    num1 = int(input("Enter Number 1: "))
+    num2 = int(input("Enter Number 2: "))
+    sum = num1 + num2
+    print("Adding the two numbers gives you: ", sum)
+    num3 = int(input("Enter Number 3: "))
+    num4 = int(input("Enter Number 4: "))
+    total = num3 + num4
+    print("Adding num3 and num4 gives you: ", total)
+
+
+sum()
